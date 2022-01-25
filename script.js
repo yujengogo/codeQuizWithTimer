c557c0ad263bd11f02323c70ea306cfbcc77a72e
Yujen Chen <yujengogo@gmail.com>
2022-01-24 22:21:02 -0500

    adding 3 questions

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ function genQuiz(questions, quizContainer, resultsContainer, SubButton) {
         showResults(questions, quizContainer,resultsContainer);
     }
 }
+var myQs = [
+    {
+        question: "What is not a program language?",
+        answers: {
+            a: 'Javascript',
+            b: 'Ruby',
+            c: 'Python',
+            d: 'Pikachu'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "What does the following expression return? typeof(3);",
+        answers: {
+            a: "Array",
+            b: "Scope",
+            c: "Number",
+            d: "Object",
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "What does the following expression return? NaN === NaN",
+        answers: {
+            a: 'NaN',
+            b: 'True',
+            c: 'False',
+            d: 'somewhat the same'
+        },
+        correctAnswer: 'c'
+    },
+]
